7ed942624ba0eb4188e08e9122cdb3ef2a51604e
masud467 <masudpfrnt@gmail.com>
2025-01-14 15:43:41 +0600

    updated navbar section ui interface.

--- src/components/Shared/Navbar.jsx
+++ src/components/Shared/Navbar.jsx
@@ -1,9 +1,34 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
+import { IoCartOutline, IoSearch } from "react-icons/io5";
 
 const Navbar = () => {
+  const navItems = [
+    {
+      title: "Home",
+      path: "/",
+    },
+    {
+      title: "About",
+      path: "/about",
+    },
+    {
+      title: "Services",
+      path: "/services",
+    },
+    {
+      title: "Blogs",
+      path: "/blogs",
+    },
+    {
+      title: "Contact",
+      path: "/contact",
+    },
+  ];
   return (
-    <div>
-      <div className="navbar bg-base-100">
+    <div className="bg-slate-200">
+      <div className="navbar container mx-auto">
         <div className="navbar-start">
           <div className="dropdown">
             <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -24,54 +49,42 @@ const Navbar = () => {
             </div>
             <ul
               tabIndex={0}
-              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-4 shadow gap-2"
             >
-              <li>
-                <a>Item 1</a>
-              </li>
-              <li>
-                <a>Parent</a>
-                <ul className="p-2">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
-              </li>
-              <li>
-                <a>Item 3</a>
-              </li>
+              {navItems.map((item) => (
+                <Link
+                  className="font-semibold hover:text-primary duration-300 hover:underline"
+                  href={item.path}
+                  key={item.path}
+                >
+                  {item.title}
+                </Link>
+              ))}
             </ul>
           </div>
-          <a className="btn btn-ghost text-xl">daisyUI</a>
+          <Link href={"/"}>
+            <Image src="/assets/logo.svg" width={50} height={100} alt="logo" />
+          </Link>
         </div>
-        <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal px-1">
-            <li>
-              <a>Item 1</a>
-            </li>
-            <li>
-              <details>
-                <summary>Parent</summary>
-                <ul className="p-2">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
-              </details>
-            </li>
-            <li>
-              <a>Item 3</a>
-            </li>
-          </ul>
+        <div className="navbar-center hidden lg:flex gap-5 items-center justify-center">
+          {navItems.map((item) => (
+            <Link
+              className="font-semibold hover:text-primary duration-300 hover:underline"
+              href={item.path}
+              key={item.path}
+            >
+              {item.title}
+            </Link>
+          ))}
         </div>
         <div className="navbar-end">
-          <a className="btn">Button</a>
+          <div className="flex items-center gap-5">
+            <IoCartOutline className="text-2xl"/>
+            <IoSearch className="text-2xl"/>
+            <a className="btn btn-outline btn-primary text-white">
+              appointment
+            </a>
+          </div>
         </div>
       </div>
     </div>
