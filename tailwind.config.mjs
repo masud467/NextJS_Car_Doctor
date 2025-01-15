be0869d121e904fce73fd30cc69bde91c61d9aa6
masud467 <masudpfrnt@gmail.com>
2025-01-15 20:00:44 +0600

    install daisy ui and updated tailwind config and next config files.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -13,7 +13,20 @@ export default {
       },
     },
   },
-  plugins: [
-    require('daisyui'),
-  ],
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        carDoctorTheme: {
+          primary: "#FF3811",
+          secondary: "#f6d860",
+          accent: "#37cdbe",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
 };
