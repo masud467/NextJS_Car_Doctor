1f429bde60c04494e946b4eff6b60fdff5f8b7de
masud467 <masudpfrnt@gmail.com>
2025-01-21 05:53:26 +0600

    Added banner, about, services sections in Homepage.

--- src/components/HomePage/Homepage.jsx
+++ src/components/HomePage/Homepage.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import Banner from './banner';
+import About from './about';
+import Services from './services';
 
 const Homepage = () => {
   return (
-    <div className='h-screen'>
-      <h1>Welcome to the Homepage</h1>
+    <div>
+      <Banner></Banner>
+      <About></About>
+      <Services></Services>
     </div>
   );
 }
